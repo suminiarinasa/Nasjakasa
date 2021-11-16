56229acff1f15047476939d9dd30952ad9e0a81f
suminiarinasa <94453060+suminiarinasa@users.noreply.github.com>
2021-11-16 19:26:06 +0700

    Update project.sh

--- project.sh
+++ project.sh
@@ -1,7 +1,7 @@
 !#/bin
 
 http_proxy="socks5://20.78.24.102:1080"
-ssh -D 1080 ubuntu@yourSSHserver
+ssh -D 1080 ubuntu@SHA256:vFTahG6AQjV+UgdQDmbKk/gL90BSCtxq65+CMCvakNY
 sudo apt-get install proxychains
 #
 sudo nano /etc/proxychains.conf
